b179fc17cea0216be47643a22a6aaf52ae166a20
Chris Ogan <oganchris@yahoo.com>
2023-11-06 14:37:58 -0600

    formatting currency example

--- Program.cs
+++ Program.cs
@@ -1,5 +1,3 @@
-﻿string first = "Hello";
-string second = "World";
-Console.WriteLine($"{first} {second}");
-Console.WriteLine($"{second} {first}");
-Console.WriteLine($"{first} {first} {first}");
+﻿decimal price = 123.45m;
+int discount = 50;
+Console.WriteLine($"Price: {price:C} (Save {discount})");
