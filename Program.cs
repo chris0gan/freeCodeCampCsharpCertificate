f5bdb7f2c247aecb33fd2557a6ea94564ab70243
Chris Ogan <oganchris@yahoo.com>
2023-11-06 13:00:56 -0600

    updated to add "," in between each value

--- Program.cs
+++ Program.cs
@@ -43,5 +43,6 @@
 string value = "abc123";
 char[] valueArray = value.ToCharArray();
 Array.Reverse(valueArray);
-string result = new string(valueArray);
+//string result = new string(valueArray);
+string result = String.Join(",", valueArray);
 Console.WriteLine(result);
