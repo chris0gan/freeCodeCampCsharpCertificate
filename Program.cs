d40b6216ddcaaa39f2dc5c438d79530d504395fc
Chris Ogan <oganchris@yahoo.com>
2023-11-06 14:33:39 -0600

    composite formatting cont.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
 ﻿string first = "Hello";
 string second = "World";
-string result = string.Format("{0} {1}!", first,second);
-Console.WriteLine(result);
+Console.WriteLine("{1} {0}!", first, second);
+Console.WriteLine("{0} {0} {0}!", first, second);
