8d0c58fcb1c7370f69c480ae819b3e924c169aec
Chris Ogan <oganchris@yahoo.com>
2023-11-06 14:26:27 -0600

    step 8 for operations on arrays

--- Program.cs
+++ Program.cs
@@ -1,19 +1,11 @@
-﻿string pangram = "The quick brown fox jumps over the lazy dog";
+﻿string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+string[] items = orderStream.Split(',');
+Array.Sort(items);
 
-// Step 1
-string[] message = pangram.Split(' ');
-
-//Step 2
-string[] newMessage = new string[message.Length];
-
-// Step 3
-for (int i = 0; i < message.Length; i++)
-{
-    char[] letters = message[i].ToCharArray();
-    Array.Reverse(letters);
-    newMessage[i] = new string(letters);
+foreach(var item in items) {
+    if(item.Length == 4) {
+        Console.WriteLine(item);
+    } else {
+        Console.WriteLine(item + "\t- Error");
+    }
 }
-
-//Step 4
-string result = String.Join(" ", newMessage);
-Console.WriteLine(result);
