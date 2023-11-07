3edc94b7ee778a39c0836216e97bfc5e4fb48caa
Chris Ogan <oganchris@yahoo.com>
2023-11-06 21:32:03 -0600

    retrieve all instances inside parentheses

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
-﻿string message = "Find what is (inside the parantheses)";
+﻿string message = "(What if) there are (more than) one (set of parentheses)?";
+while (true)
+{
+    int openingPosition = message.IndexOf('(');
+    if (openingPosition == -1) break;
 
-int openingPosition = message.IndexOf("(");
-int closingPosition = message.IndexOf(")");
+    openingPosition += 1;
+    int closingPosition = message.IndexOf(')');
+    int length = closingPosition - openingPosition;
+    Console.WriteLine(message.Substring(openingPosition, length));
 
-// Console.WriteLine(openingPosition);
-// Console.WriteLine(closingPosition);
-
-int length = closingPosition - openingPosition;
-Console.WriteLine(message.Substring(openingPosition, length));
+    // Note the overload of the Substring to return only the remaining 
+    // unprocessed message:
+    message = message.Substring(closingPosition + 1);
+}
