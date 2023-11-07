00ac4ae025204e0dcd6d6daf07516115d114305c
Chris Ogan <oganchris@yahoo.com>
2023-11-07 17:04:16 -0600

    added validateRange()

--- Program.cs
+++ Program.cs
@@ -27,4 +27,17 @@ void ValidateZeroes() {
 
     validZeroes = true;
 }
-void ValidateRange() {}
+void ValidateRange() {
+    string[] address = ipv4Input.Split(".");
+
+    foreach (string number in address) 
+    {
+        int value = int.Parse(number);
+        if (value < 0 || value > 255) 
+        {
+            validRange = false;
+            return;
+        }
+    }
+    validRange = true;
+}
