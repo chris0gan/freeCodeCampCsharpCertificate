4dc3ec78d09b58fdc8c111e835caefec40042a99
Chris Ogan <oganchris@yahoo.com>
2023-11-07 15:01:24 -0600

    added case "2" logic

--- Program.cs
+++ Program.cs
@@ -127,7 +127,18 @@ do
 
         case "2":
             // Display all dogs with a specified characteristic
-            Console.WriteLine("\nUNDER CONSTRUCTION - please check back next month to see progress.");
+            //Console.WriteLine("\nUNDER CONSTRUCTION - please check back next month to see progress.");
+            string dogCharcteristic = "";
+
+            while(dogCharcteristic == "") {
+                //having user enter physical charctersitics to search for
+                Console.WriteLine($"\nEnter one desired dog characteristics to search for");
+                readResult = Console.ReadLine();
+                if(readResult != null) {
+                    dogCharcteristic = readResult.ToLower().Trim();
+                }
+            }
+
             Console.WriteLine("Press the Enter key to continue.");
             readResult = Console.ReadLine();
             break;
