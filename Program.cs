8d1f26fe0652668f457f894b340f24a8c604b6a3
Chris Ogan <oganchris@yahoo.com>
2023-11-10 17:34:59 -0600

    answer to display email addresses

--- Program.cs
+++ Program.cs
@@ -1,47 +1,30 @@
-﻿string[] guestList = {"Rebecca", "Nadia", "Noor", "Jonte"};
-string[] rsvps = new string[10];
-int count = 0;
+﻿string[,] corporate = 
+	{
+		{"Robert", "Bavin"}, {"Simon", "Bright"},
+		{"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+		{"Sarah", "Delucchi"}, {"Sinan", "Ali"}};
 
-RSVP("Rebecca");
-RSVP("Nadia", 2, "Nuts");
-RSVP(name:"Linh", partySize: 2, inviteOnly: false);
-RSVP("Tony", allergies: "Jackfruit", inviteOnly: true);
-RSVP("Noor", 4, inviteOnly: false);
-RSVP("Jonte", 2, "Stone fruit", false);
-ShowRSVPs();
+	string[,] external = 
+	{
+		{"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+		{"Shay", "Lawrence"}, {"Daren", "Valdes"}
+	};
 
-void RSVP(string name, int partySize = 1, string allergies = "none", bool inviteOnly = true) 
-{
-    if (inviteOnly)
-    {
-        // search guestList before adding rsvp
-        if(inviteOnly) {
-            bool found = false;
+	string externalDomain = "hayworth.com";
 
-            foreach(string guest in guestList) {
-                if(guest.Equals(name)) {
-                    found = true;
-                    break;
-                }
-            }
-            if(!found) {
-                Console.WriteLine($"Sorry, {name} is not on the guest list");
-                return;
-            }
-        }
-    }
+	for (int i = 0; i < corporate.GetLength(0); i++) 
+	{
+		DisplayEmail(first: corporate[i,0], last: corporate[i,1]);
+	}
 
-    rsvps[count] = $"Name: {name}, \tParty Size: {partySize}, \tAllergies: {allergies}";
-    count++;
-}
+	for (int i = 0; i < external.GetLength(0); i++) 
+	{
+		DisplayEmail(first: external[i,0], last: external[i,1], domain: externalDomain);
+	}
 
-
-
-void ShowRSVPs()
-{
-    Console.WriteLine("\nTotal RSVPs:");
-    for (int i = 0; i < count; i++)
-    {
-        Console.WriteLine(rsvps[i]);
-    }
-}
+	void DisplayEmail(string first, string last, string domain = "contoso.com") 
+	{
+		string email = first.Substring(0, 2) + last;
+		email = email.ToLower();
+		Console.WriteLine($"{email}@{domain}");
+	}
