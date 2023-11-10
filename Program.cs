b224755eb9fe5eb5e05f7e53ae5873f52766814d
Chris Ogan <oganchris@yahoo.com>
2023-11-10 10:47:54 -0600

    create a method with parameters intro

--- Program.cs
+++ Program.cs
@@ -1,43 +1,5 @@
-﻿
-string ipv4Input = "107.31.1.5";
-bool validLength = false;
-bool validZeroes = false;
-bool validRange = false;
+﻿int [] schedule = {800,1200,1600,2000};
 
-if(ValidateLength() && ValidateZeroes() && ValidateRange()) {
-    Console.WriteLine($"id is a valid IPv4 address");
-} else {
-    Console.WriteLine($"ip is an invalid IPv4 address");
-}
-
-void ValidateLength() {
-    string[] address = ipv4Input.Split(".");
-    validLength = address.Length == 4;
-}
-void ValidateZeroes() {
-    string[] address = ipv4Input.Split(".");
-
-    foreach (string number in address) 
-    {
-        if (number.Length > 1 && number.StartsWith("0")) 
-        {
-            validZeroes = false;
-        }
-    }
-
-    validZeroes = true;
-}
-void ValidateRange() {
-    string[] address = ipv4Input.Split(".");
-
-    foreach (string number in address) 
-    {
-        int value = int.Parse(number);
-        if (value < 0 || value > 255) 
-        {
-            validRange = false;
-            return;
-        }
-    }
-    validRange = true;
+void DisplayAdjustedTimes(int[] times, int currentGMT, int newGMT) {
+    
 }
