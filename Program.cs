1b61e2d619ffdbbeb51038f823ceea90a617cd23
Chris Ogan <oganchris@yahoo.com>
2023-11-10 13:14:37 -0600

    using named arguments

--- Program.cs
+++ Program.cs
@@ -1,11 +1,45 @@
-﻿string status = "Healthy";
+﻿string[] guestList = {"Rebecca", "Nadia", "Noor", "Jonte"};
+string[] rsvps = new string[10];
+int count = 0;
 
-Console.WriteLine($"Start: {status}");
-SetHealth(false);
-Console.WriteLine($"End: {status}");
+RSVP("Rebecca", 1, "none", true);
+RSVP("Nadia", 2, "Nuts", true);
+RSVP(name:"Linh", partySize: 2, allergies: "none", inviteOnly: false);
+RSVP("Tony", inviteOnly: true, allergies: "Jackfruit",  partySize: 1);
+RSVP("Noor", 4, "none", false);
+RSVP("Jonte", 2, "Stone fruit", false);
+ShowRSVPs();
 
-void SetHealth(bool isHealthy) 
+void RSVP(string name, int partySize, string allergies, bool inviteOnly) 
 {
-    status = (isHealthy ? "Healthy" : "Unhealthy");
-    Console.WriteLine($"Middle: {status}");
+    if (inviteOnly)
+    {
+        // search guestList before adding rsvp
+        if(inviteOnly) {
+            bool found = false;
+
+            foreach(string guest in guestList) {
+                if(guest.Equals(name)) {
+                    found = true;
+                    break;
+                }
+            }
+            if(!found) {
+                Console.WriteLine($"Sorry, {name} is not on the guest list");
+                return;
+            }
+        }
+    }
+
+    rsvps[count] = $"Name: {name}, \tParty Size: {partySize}, \tAllergies: {allergies}";
+    count++;
+}
+
+void ShowRSVPs()
+{
+    Console.WriteLine("\nTotal RSVPs:");
+    for (int i = 0; i < count; i++)
+    {
+        Console.WriteLine(rsvps[i]);
+    }
 }
