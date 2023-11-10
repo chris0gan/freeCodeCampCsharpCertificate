4de0566437f6dd221943905ad5e2b1d941f9a239
Chris Ogan <oganchris@yahoo.com>
2023-11-10 11:45:21 -0600

    test pass by value commit

--- Program.cs
+++ Program.cs
@@ -1,24 +1,11 @@
-﻿
+﻿int a = 3;
+int b = 4;
+int c = 0;
 
+Multiply(a, b, c);
+Console.WriteLine($"global statement: {a} x {b} = {c}");
 
-double pi = 3.14159;
-PrintCircleInfo(12);
-PrintCircleInfo(24);
-
-void PrintCircleInfo(int radius) {
-    Console.WriteLine($"Circle with radius {radius}");
-    PrintCircleArea(radius);
-    PrintCircleCircumference(radius);
-}
-
-void PrintCircleArea(int radius)
-{
-    double area = pi * (radius * radius);
-    Console.WriteLine($"Area = {area}");
-}
-
-void PrintCircleCircumference(int radius) {
-    
-    double circumference = 2 * pi * radius;
-    Console.WriteLine($"Circumference = {circumference}");
+void Multiply(int a, int b, int c) {
+    c = a * b;
+    Console.WriteLine($"inside Multiply method: {a} x {b} = {c}");
 }
