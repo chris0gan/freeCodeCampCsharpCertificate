7d57ce17016ea9c46fc7dfab0ad96733d48c39cb
Chris Ogan <oganchris@yahoo.com>
2023-11-10 11:51:30 -0600

    test by reference commit

--- Program.cs
+++ Program.cs
@@ -1,11 +1,20 @@
-﻿int a = 3;
-int b = 4;
-int c = 0;
+﻿using System.Runtime.InteropServices;
 
-Multiply(a, b, c);
-Console.WriteLine($"global statement: {a} x {b} = {c}");
+int [] array = {1,2,3,4,5};
 
-void Multiply(int a, int b, int c) {
-    c = a * b;
-    Console.WriteLine($"inside Multiply method: {a} x {b} = {c}");
+PrintArray(array);
+Clear(array);
+PrintArray(array);
+
+void PrintArray(int [] array) {
+    foreach(int a in array) {
+        Console.Write($"{a} ");
+    }
+    Console.WriteLine();
+}
+
+void Clear(int[] array) {
+    for(int i = 0; i < array.Length; i++) {
+        array[i] = 0;
+    }
 }
