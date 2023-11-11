5afc8edf35da40a3485e30ceae28fe0495cd7fda
Chris Ogan <oganchris@yahoo.com>
2023-11-10 22:51:08 -0600

    testing ReverseWord method

--- Program.cs
+++ Program.cs
@@ -1,5 +1,14 @@
-﻿string ReverseWord(string word) {
+﻿string input = "snake";
+
+Console.WriteLine(input);
+Console.WriteLine(ReverseWord(input));
+
+string ReverseWord(string word) {
     string result = "";
+
+    for(int i = word.Length - 1; i >= 0; i--) {
+        result += word[i];
+    }
 
     return result;
 }
