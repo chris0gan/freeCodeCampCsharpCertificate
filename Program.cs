5963e81c34baa3d4efa38db0132cf68706abf413
Chris Ogan <oganchris@yahoo.com>
2023-11-12 13:00:32 -0600

    excersize complete w/ test

--- Program.cs
+++ Program.cs
@@ -6,24 +6,44 @@ if (ShouldPlay())
     PlayGame();
 }
 
+bool ShouldPlay() 
+{
+    string response = Console.ReadLine();
+    return response.ToLower().Equals("y");
+}
+
 void PlayGame() 
 {
     var play = true;
 
-    while (play) 
-    {
-        var target;
-        var roll;
+    while (play) {
+        var target = GetTarget();
+        var roll = RollDice();
 
         Console.WriteLine($"Roll a number greater than {target} to win!");
         Console.WriteLine($"You rolled a {roll}");
-        Console.WriteLine(WinOrLose());
+        Console.WriteLine(WinOrLose(roll, target));
         Console.WriteLine("\nPlay again? (Y/N)");
 
         play = ShouldPlay();
     }
 }
 
-bool ShouldPlay() {
-    return true;
+int GetTarget() 
+{
+    return random.Next(1, 6);
+}
+
+int RollDice() 
+{
+    return random.Next(1, 7);
+}
+
+string WinOrLose(int roll, int target) 
+{
+    if (roll > target) 
+    {
+        return "You win!";
+    }
+    return "You lose!";
 }
