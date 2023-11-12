9aeaa146de44c3dc69ffd5580a78ca201c662a7a
Chris Ogan <oganchris@yahoo.com>
2023-11-12 17:14:44 -0600

    adding logic to AssignGoup method

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using System;
 
 string[] pettingZoo = 
 {
@@ -8,18 +8,36 @@ string[] pettingZoo =
 };
 
 RandomizeAnimals();
-// string[,] group = AssignGroup();
+string[,] group = AssignGroup();
 Console.WriteLine("School A");
 // PrintGroup(group);
 
-void RandomizeAnimals() {
+void RandomizeAnimals() 
+{
     Random random = new Random();
 
-    for(int i = 0; i < pettingZoo.Length; i++) {
+    for (int i = 0; i < pettingZoo.Length; i++) 
+    {
         int r = random.Next(i, pettingZoo.Length);
 
-        string temp = pettingZoo[i];
-        pettingZoo[i] = pettingZoo[r];
-        pettingZoo[r] = temp;
+        string temp = pettingZoo[r];
+        pettingZoo[r] = pettingZoo[i];
+        pettingZoo[i] = temp;
     }
+}
+
+string[,] AssignGroup(int groups = 6) 
+{
+    string[,] result = new string[groups, pettingZoo.Length/groups];
+    int start = 0;
+
+    for (int i = 0; i < groups; i++) 
+    {
+        for (int j = 0; j < result.GetLength(1); j++) 
+        {
+            result[i,j] = pettingZoo[start++];
+        }
+    }
+
+    return result;
 }
