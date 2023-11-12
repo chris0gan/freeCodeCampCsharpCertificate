d0df35e5f5a72089e9395dedb8371aacb13ef012
Chris Ogan <oganchris@yahoo.com>
2023-11-12 17:28:32 -0600

    completed excersie with  runs

--- Program.cs
+++ Program.cs
@@ -7,10 +7,18 @@ string[] pettingZoo =
     "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
 };
 
-RandomizeAnimals();
-string[,] group = AssignGroup();
-Console.WriteLine("School A");
-// PrintGroup(group);
+
+PlanSchoolVisit("School A");
+PlanSchoolVisit("School B", 3);
+PlanSchoolVisit("School C", 2);
+
+void PlanSchoolVisit(string schoolName, int groups = 6) 
+{
+    RandomizeAnimals();
+    string[,] group1 = AssignGroup(groups);
+    Console.WriteLine(schoolName);
+    PrintGroup(group1);
+}
 
 void RandomizeAnimals() 
 {
@@ -40,4 +48,17 @@ string[,] AssignGroup(int groups = 6)
     }
 
     return result;
+}
+
+void PrintGroup(string[,] group)
+{
+    for(int i = 0; i < group.GetLength(0); i++) 
+    {
+        Console.Write($"Group {i + 1}: ");
+        for(int j = 0; j < group.GetLength(1); j++) 
+        {
+            Console.Write($"{group[i,j]}  ");
+        }
+        Console.WriteLine();
+    }
 }
