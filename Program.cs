1ab720d6eb1c98a7f9d23cf324ce1b7c9082db48
Chris Ogan <oganchris@yahoo.com>
2023-11-12 14:32:29 -0600

    guided project, petting zoo

--- Program.cs
+++ Program.cs
@@ -1,49 +1,8 @@
-﻿Random random = new Random();
+﻿using System;
 
-Console.WriteLine("Would you like to play? (Y/N)");
-if (ShouldPlay()) 
+string[] pettingZoo = 
 {
-    PlayGame();
-}
-
-bool ShouldPlay() 
-{
-    string response = Console.ReadLine();
-    return response.ToLower().Equals("y");
-}
-
-void PlayGame() 
-{
-    var play = true;
-
-    while (play) {
-        var target = GetTarget();
-        var roll = RollDice();
-
-        Console.WriteLine($"Roll a number greater than {target} to win!");
-        Console.WriteLine($"You rolled a {roll}");
-        Console.WriteLine(WinOrLose(roll, target));
-        Console.WriteLine("\nPlay again? (Y/N)");
-
-        play = ShouldPlay();
-    }
-}
-
-int GetTarget() 
-{
-    return random.Next(1, 6);
-}
-
-int RollDice() 
-{
-    return random.Next(1, 7);
-}
-
-string WinOrLose(int roll, int target) 
-{
-    if (roll > target) 
-    {
-        return "You win!";
-    }
-    return "You lose!";
-}
+    "alpacas", "capybaras", "chickens", "ducks", "emus", "geese", 
+    "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws", 
+    "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
+};
